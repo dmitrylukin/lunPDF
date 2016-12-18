8388c93815bf81884e8525e89d9544621b407201
dmitrylukin <dimitry.lukin@gmail.com>
2016-12-18 04:41:44 +0300

    prealpha, small README changes

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # lunPDF
-pure lua pdf splitter
+pure lua pdf page splitter
 PyPDF2 based 
 https://github.com/mstamy2/PyPDF2
 
+Run:
+lua pdf.lua <YOURNAME>.pdf
+as a result you get pdf files with <YOURNAME>-N.pdf names and quantity equals number of pages in source pdf file. 
---
+++ pdfsplit.lua
@@ -0,0 +1,167 @@
+-------------------------------------
+--@author dimitry.lukin@gmail.com
+
+debug = true
+
+local function copyTable(original)
+    local copy = {}
+    for k, v in pairs(original) do
+           if type(v) == 'table' then
+                v = copyTable(v)
+           end
+           copy[k] = v
+    end
+    return copy
+ end
+
+PdfFile = {}
+
+PdfFile.Trailer = nil
+PdfFile.NumPages = nil
+PdfFile.Structure = {}
+PdfFile.Offset = {}
+PdfFile.Stream = {}
+PdfFile.ObjectCounter = 0
+PdfFile.RootObject = 0
+PdfFile.PagesObject = 0
+PdfFile.PagesKids = {}
+
+
+PdfFile.Read = function(filename)
+    local fh = assert(io.input(filename))
+    return fh
+end
+
+PdfFile.Parse = function(fh)
+    local structure = {}
+    while true do
+        local line = io.read()
+        if line == nil or line == "%%EOF" then break end
+        local n, sn = line:match("(%d+)%s+(%d+)%s+obj")
+        if n then
+            local isObject = true
+            PdfFile.ObjectCounter = PdfFile.ObjectCounter + 1
+            n = tonumber(n)
+            structure[n] = ""
+--            if debug then print("Found Object "..n) end
+            while isObject do
+                line = io.read()
+                if line == nil then break end
+                if not line:match("endobj") then
+                     if line:match("^stream") then
+                        local StartStreamPos = io.input():seek()
+                        while true do
+                            if io.read():match("endstream")  then break end
+                        end
+                        local StopStreamPos = io.input():seek() - 9
+                        local binfh = io.open(arg[1], "rb")
+                        binfh:seek("set", StartStreamPos)
+                        PdfFile.Stream[n] = binfh:read(StopStreamPos - StartStreamPos - 1)
+                        binfh:close()
+                      end
+                    structure[n] = structure[n]..line.."\n"
+                else
+                    isObject = false
+                end
+            end
+--            if debug then print(structure[n]) end
+        end -- end if obj found
+        if line:match("trailer") then
+        trailer = ""
+            while true do
+                line = io.read()
+                trsubst = {"(/Size%s%d+)", "(/Root%s+%d+%s+%d+%s+R)", "(/Info%s+%d+%s+%d+%s+R)", }
+                for _, i in pairs(trsubst) do
+                    
+                    local trout = line:match(i) or ""
+                    if trout ~= "" then trout = trout.."\n" end
+                    trailer = trailer..trout
+                end
+                if line:match(">>") then
+                    break
+                end
+            end
+        end 
+    end -- main loop
+
+    return trailer, structure
+end 
+
+fh = PdfFile.Read(arg[1])
+PdfFile.Trailer, PdfFile.Structure = PdfFile.Parse(fh)
+
+-- if debug then print("Trailer: "..PdfFile.Trailer) end
+-----------------------
+-- Get root catalog
+PdfFile.RootObject = PdfFile.Trailer:match("/Root%s+(%d+)%s+%d+")
+if debug then print(RootCatalog) end
+-----------------------
+-- Get page number
+PdfFile.RootObject = tonumber(PdfFile.RootObject)
+PdfFile.PagesObject = PdfFile.Structure[PdfFile.RootObject]:match("/Pages%s(%d+)%s%d%sR")
+if debug then print(PdfFile.PagesObject) end
+PdfFile.PagesObject = tonumber(PdfFile.PagesObject)
+PdfFile.NumPages = PdfFile.Structure[PdfFile.PagesObject]:match("/Count%s(%d+)")
+if debug then print("Number of pages in pdf: "..PdfFile.NumPages) end
+
+PdfFile.NumPages = tonumber(PdfFile.NumPages)
+if PdfFile.NumPages == 1 then 
+    print("There is one page pdf document. No changes needed.") 
+    return 0
+end
+---------------------------
+-- Create output files
+for pageCounter = 1, PdfFile.NumPages do
+    -- create filename 
+    local fname = arg[1]:match("(%a+).pdf")
+    fname = fname.."-"..pageCounter..".pdf"
+    local wfh=io.open(fname,"wb")
+---------------------------------------
+-- Creating output PdfFile
+    
+    local PdfFile2Write = copyTable(PdfFile)
+    local a = PdfFile.Structure[PdfFile.PagesObject]:gsub("/Count%s+%d+", "/Count 1")
+    PdfFile2Write.Structure[PdfFile2Write.PagesObject] = a
+    local b = PdfFile2Write.Structure[PdfFile2Write.PagesObject]:match("/Kids%[(.+)%]")
+    for k = 1, pageCounter - 1 do 
+        b = b:gsub("^%s*%d+%s+%d+%s+%a+", " ", 1)
+    end
+    PdfFile.PagesKids[pageCounter] = b:match("^%s*(%d+)%s+0%s+R")      
+    z = a:gsub("/Kids%[(.+)%]", "/Kids[ "..PdfFile.PagesKids[pageCounter].." 0 R ]")
+    PdfFile2Write.Structure[PdfFile2Write.PagesObject] = z
+
+---------------------------------------
+-- Forming page
+    -- write header
+    wfh:write("%PDF-1.3\n")
+    wfh:write("%\208\206\165\178\n")
+    -- write objects
+    for i,data in pairs(PdfFile2Write.Structure) do
+        PdfFile2Write.Offset[i] = wfh:seek()
+        wfh:write(i.." 0 obj\n")
+        wfh:write(PdfFile2Write.Structure[i])
+        if PdfFile2Write.Stream[i] ~=nil then 
+            wfh:seek("end")
+            wfh:write(PdfFile2Write.Stream[i])
+            wfh:write("endstream\n")
+        end
+        wfh:write("endobj\n\n")
+    end
+    -- write trailer
+    local xferOffset = wfh:seek()
+    wfh:write("xref\n")
+    wfh:write("0 "..tostring(PdfFile2Write.ObjectCounter + 1).."\n")
+    wfh:write("0000000000 65535 f\n")
+    for i in pairs(PdfFile2Write.Structure) do
+        o = string.format("%010d", PdfFile2Write.Offset[i])
+        wfh:write(o.." 00000 n\n")
+    end
+    wfh:write("trailer\n<<\n")
+    wfh:write(PdfFile2Write.Trailer..">>\n")
+    -- write footer 
+    wfh:write("startxref\n"..xferOffset.."\n")
+    wfh:write("%%EOF\n")
+    wfh:close()
+end -- end write file loop
+
+
