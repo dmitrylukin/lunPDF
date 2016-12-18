bf316efde60c78d3e66bb4ef758be84a9a42c46f
dmitrylukin <dimitry.lukin@gmail.com>
2016-12-18 19:12:50 +0300

    scanner pdf processing

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # lunPDF
-pure lua pdf page splitter
-PyPDF2 based 
-https://github.com/mstamy2/PyPDF2
+## pure lua pdf page splitter 
+
+As help material I used a PyPDF2 sources  
+https://github.com/mstamy2/PyPDF2 
+and ISO32000
+https://en.wikipedia.org/wiki/Portable_Document_Format
 
 Run:
-lua pdfsplit.lua <YOURNAME>.pdf
-as a result you get pdf files with <YOURNAME>-N.pdf names. Where N is number from 1 to number of pages in source pdf file.
+```
+lua pdfsplit.lua YOURNAME.pdf
+```
+as a result you get pdf files with YOURNAME-N.pdf names. Where N is number from 1 to number of pages in source pdf file.
+Splitter is absolutlely not universal and tested only on LibreOffice and unknown scanner pdf docs.
+
--- pdfsplit.lua
+++ pdfsplit.lua
@@ -3,6 +3,15 @@
 
 debug = true
 
+local function testUnnecessary(tab, element, list)
+    for i,v in pairs(list) do
+        if tonumber(v) == tonumber(element) then
+            return true
+        end
+    end
+    return false
+end
+
 local function copyTable(original)
     local copy = {}
     for k, v in pairs(original) do
@@ -25,6 +34,8 @@ PdfFile.ObjectCounter = 0
 PdfFile.RootObject = 0
 PdfFile.PagesObject = 0
 PdfFile.PagesKids = {}
+PdfFile.Resources = {}
+PdfFile.ImagesObjects = {}
 
 
 PdfFile.Read = function(filename)
@@ -94,12 +105,12 @@ PdfFile.Trailer, PdfFile.Structure = PdfFile.Parse(fh)
 -----------------------
 -- Get root catalog
 PdfFile.RootObject = PdfFile.Trailer:match("/Root%s+(%d+)%s+%d+")
-if debug then print(RootCatalog) end
+if debug then print("RootCatalog object:  "..PdfFile.RootObject) end
 -----------------------
 -- Get page number
 PdfFile.RootObject = tonumber(PdfFile.RootObject)
 PdfFile.PagesObject = PdfFile.Structure[PdfFile.RootObject]:match("/Pages%s(%d+)%s%d%sR")
-if debug then print(PdfFile.PagesObject) end
+if debug then print("Pages object:  "..PdfFile.PagesObject) end
 PdfFile.PagesObject = tonumber(PdfFile.PagesObject)
 PdfFile.NumPages = PdfFile.Structure[PdfFile.PagesObject]:match("/Count%s(%d+)")
 if debug then print("Number of pages in pdf: "..PdfFile.NumPages) end
@@ -120,22 +131,36 @@ for pageCounter = 1, PdfFile.NumPages do
 -- Creating output PdfFile
     
     local PdfFile2Write = copyTable(PdfFile)
+-- Write /Count 1 to Pages obj
     local a = PdfFile.Structure[PdfFile.PagesObject]:gsub("/Count%s+%d+", "/Count 1")
     PdfFile2Write.Structure[PdfFile2Write.PagesObject] = a
+-- 
     local tmp = PdfFile2Write.Structure[PdfFile2Write.PagesObject]:gsub("\n","")
---    local b = PdfFile2Write.Structure[PdfFile2Write.PagesObject]:match("/Kids%[(.+)%]")
-    local b = tmp:match("/Kids%s*%[(.+)%]")
---print("Befote:   "..b)
+    local b = tmp:match("/Kids%s*%[(.+)%]") -- b ia a list of Kids in form "12 0 R 2 0 R 7 0 R"  and so
+    local kids = b
     for k = 1, pageCounter - 1 do 
-        b = b:gsub("^%s*%d+%s+%d+%s+%a+", " ", 1)
+        b = b:gsub("^%s*%d+%s+%d+%s+%a+", " ", 1) -- b is a 
     end
---print("After: "..b)
     PdfFile.PagesKids[pageCounter] = b:match("^%s*(%d+)%s+0%s+R")      
---print("Kids: "..PdfFile.PagesKids[pageCounter])
+-- 
     z = tmp:gsub("/Kids%s*%[(.+)%]", "/Kids[ "..PdfFile.PagesKids[pageCounter].." 0 R ]")
---print("Result:"..z)
     PdfFile2Write.Structure[PdfFile2Write.PagesObject] = z
-
+--------------------------------------
+-- Try to remove unused objects
+    unnecessaryList = {}
+    while true do
+        key, tail = kids:match("^%s*(%w+)%s+(.*)$")
+        if not key then break end
+        if not ( key == "0" or key == "R" or tonumber(key) == tonumber(PdfFile.PagesKids[pageCounter]) ) then
+            local tmp = PdfFile2Write.Structure[tonumber(key)]:gsub("\n", " ")
+            local page = tmp:match("/Resources%s+(%d+)%s+%d+%s+R")
+            local imgPage = PdfFile2Write.Structure[tonumber(page)]:gsub("\n", " ")
+            local imgObj = imgPage:match("/Im0%s+(%d+)%s+%d+%s+R")
+            table.insert(unnecessaryList, imgObj)
+        end
+        kids = tail
+    end
+--    removeUnnecessary(PdfFile2Write, unnecessaryList)
 ---------------------------------------
 -- Forming page
     -- write header
@@ -143,24 +168,29 @@ for pageCounter = 1, PdfFile.NumPages do
     wfh:write("%\208\206\165\178\n")
     -- write objects
     for i,data in pairs(PdfFile2Write.Structure) do
-        PdfFile2Write.Offset[i] = wfh:seek()
-        wfh:write(i.." 0 obj\n")
-        wfh:write(PdfFile2Write.Structure[i])
-        if PdfFile2Write.Stream[i] ~=nil then 
-            wfh:seek("end")
-            wfh:write(PdfFile2Write.Stream[i])
-            wfh:write("endstream\n")
-        end
-        wfh:write("endobj\n\n")
+        if not testUnnecessary(PdfFile2Write, i, unnecessaryList) then 
+print("obj number  "..i)
+            PdfFile2Write.Offset[i] = wfh:seek()
+            wfh:write(i.." 0 obj\n")    
+            wfh:write(PdfFile2Write.Structure[i])
+            if PdfFile2Write.Stream[i] ~=nil then 
+                wfh:seek("end")
+                wfh:write(PdfFile2Write.Stream[i])
+                wfh:write("endstream\n")
+            end
+            wfh:write("endobj\n\n")
+        end -- end if test unnecessaty
     end
-    -- write trailer
+   -- write trailer
     local xferOffset = wfh:seek()
     wfh:write("xref\n")
     wfh:write("0 "..tostring(PdfFile2Write.ObjectCounter + 1).."\n")
     wfh:write("0000000000 65535 f\n")
     for i in pairs(PdfFile2Write.Structure) do
-        o = string.format("%010d", PdfFile2Write.Offset[i])
-        wfh:write(o.." 00000 n\n")
+        if not testUnnecessary(PdfFile2Write, i, unnecessaryList) then 
+            o = string.format("%010d", PdfFile2Write.Offset[i])
+            wfh:write(o.." 00000 n\n")
+        end
     end
     wfh:write("trailer\n<<\n")
     wfh:write(PdfFile2Write.Trailer..">>\n")
